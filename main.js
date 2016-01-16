2cd05b5754113cc10cce597abc503855a3d489cb
Aaron Saray <me@aaronsaray.com>
2016-01-16 16:57:14 -0600

    Code challenge completed.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Cash Register Object
  * @constructor
  */
-var CashRegister  = function() {
+var CashRegister = function() {
 
     /**
      * The initial starting drawer
@@ -36,17 +36,33 @@ var CashRegister  = function() {
     ];
 
     /**
-     * Populate drawer the first run
+     * current drawer
      * @type {Object}
      */
-    var currentBalance = initialDrawer;
+    var currentDrawer = [];
 
     /**
-     * Get current balance
+     * The current balance
+     * @type int
+     */
+    var currentBalance = 0;
+
+    /**
+     * Resets the drawer
+     */
+    this.reloadDrawer = function() {
+        currentDrawer = JSON.parse(JSON.stringify(initialDrawer));
+        currentBalance = currentDrawer.reduce(function(previousValue, currentBill, idx) {
+            return previousValue + (currentBill.bill * currentBill.amount);
+        }, 0);
+    };
+
+    /**
+     * Get current drawer
      * @returns {*}
      */
-    this.getCurrentBalance = function() {
-        return currentBalance;
+    this.getCurrentDrawer = function() {
+        return currentDrawer;
     };
 
     /**
@@ -55,11 +71,11 @@ var CashRegister  = function() {
      * @param bills
      * @returns {Array}
      */
-    this.getSpecificBalances = function(bills) {
+    this.getSpecificDrawerBills = function(bills) {
         var balances = [];
         for (var i = 0, count = bills.length; i < count; i++) {
             var bill = bills[i].replace(/\D/g, '');
-            var potential = currentBalance.filter(function(billContainer) {
+            var potential = currentDrawer.filter(function(billContainer) {
                 return billContainer.bill == bill;
             });
             if (!potential.length) throw Error('The bill ' + bill + ' was not found');
@@ -70,13 +86,40 @@ var CashRegister  = function() {
     };
 
     /**
-     * Resets the drawer
+     * withdraw this amount
+     *
+     * @param amount
+     * @returns {Array}
      */
-    this.reloadDrawer = function() {
-        currentBalance = initialDrawer;
+    this.withdraw = function(amount) {
+        var total = parseInt(amount.replace(/\D/g, ''));
+        if (total > currentBalance) {
+            throw Error('insufficient funds');
+        }
+
+        var bills = [];
+        var pointer = 0;
+        while (total > 0) {
+            var currentBill = currentDrawer[pointer];
+
+            var numberOfBills = parseInt(total / currentBill.bill);
+            if (numberOfBills) {
+                currentDrawer[pointer].amount -= numberOfBills;
+                var remove = (currentBill.bill * numberOfBills);
+                total -= remove;
+                currentBalance -= remove;
+            }
+
+            pointer++;
+
+            if (pointer == currentDrawer.length) {
+                break;
+            }
+        }
+
+        return bills;
     };
 };
-
 
 /**
  * Namespaced "container"
@@ -92,6 +135,7 @@ var UserInterface = {
      */
     init: function() {
         var ui = this;
+        ui.cashRegister.reloadDrawer();
         ui.form.addEventListener('submit', function(e) {
             e.preventDefault();
             var value = this.input.value.toUpperCase();
@@ -106,30 +150,34 @@ var UserInterface = {
      */
     handleCommand: function(command) {
         var output = '';
-        switch (command.charAt(0)) {
-            case 'R':
-                this.cashRegister.reloadDrawer();
-                output = this.formatBalance(this.cashRegister.getCurrentBalance(), ['Machine balance:']);
-                break;
-            case 'I':
-                try {
-                    output = this.formatBalance(this.cashRegister.getSpecificBalances(command.substr(2).split(' ')), []);
-                }
-                catch (e) {
-                    output = 'Failure: ' + e.message;
-                }
-                break;
-            case 'Q':
-                this.quit();
-                break;
 
-            default:
-                output = 'Failure: Invalid Command';
+        try {
+            switch (command.charAt(0)) {
+                case 'R':
+                    this.cashRegister.reloadDrawer();
+                    output = this.formatBalance(this.cashRegister.getCurrentDrawer(), ['Machine balance:']);
+                    break;
+                case 'I':
+                    output = this.formatBalance(this.cashRegister.getSpecificDrawerBills(command.substr(2).split(' ')), []);
+                    break;
+                case 'W':
+                    var withdrawl = command.substr(2);
+                    this.cashRegister.withdraw(withdrawl);
+                    output = this.formatBalance(this.cashRegister.getCurrentDrawer(), ['Success: Dispensed ' + withdrawl, '', 'Machine balance:']);
+                    break;
+                case 'Q':
+                    this.quit();
+                    break;
 
+                default:
+                    throw Error('Invalid Command');
+            }
+        }
+        catch (e) {
+            output = 'Failure: ' + e.message;
         }
 
         return output;
-
     },
 
     /**
@@ -156,6 +204,5 @@ var UserInterface = {
         document.body.style.cursor = 'not-allowed';
     }
 };
-
 
 UserInterface.init();
