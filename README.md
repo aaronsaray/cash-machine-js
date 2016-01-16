596157c4b85b14f60d2acfcf2d1cf355875b0636
Aaron Saray <me@aaronsaray.com>
2016-01-16 16:06:22 -0600

    Updated the code for the first time.  a couple functions are done.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# cash-machine-js
-This is for a coding test.  Not sure what else it would be useful for besides that!
+# Javascript Cash Machine
+
+This is a coding challenge I was issued.  I've included the challenge at the bottom of this document.  
+
+## Goal
+
+My goal was to program this in pure javascript, html and css - using no external libraries.  In doing so, I'm only supporting
+recent browsers like IE11+, Firefox, Chrome and Safari.  I wanted to do this in javascript in the browser versus in something
+like PHP or nodeJS because I think it's just a fun exercise.
+
+## Official Challenge
+
+The cash machine is pre-stocked with the following denominations:
+
+- $100 - 10 Bills
+- $50 - 10 Bills
+- $20 - 10 Bills
+- $10 - 10 Bills
+- $5 - 10 Bills
+- $1 - 10 Bills
+
+Your application should take input from the standard input stream and support the following commands:
+
+- R - Restocks the cash machine to the original pre-stock levels defined above 
+- W<dollar amount>   - Withdraws that amount from the cash machine (e.g. "W $145") 
+- I<denominations>   - Displays the number of bills in that denomination present in the cash machine (e.g. I $20 $10 $1) 
+- Q - Quits the application
+
+The withdrawals from the cash machine should dispense cash in the most efficient manner possible, with the least amount of bills.  After a withdrawal, the program should display success or failure and the remaining balance in the cash machine (sample output below).  For an inquiry, the program should display the number of bills in the denominations specified (sample output below).  After a restock, the program should display the balance in the cash machine (same as after a withdrawal).  If the input is not understood, "Invalid Command" should be displayed.  No additional messages, prompts or errors should be displayed.
---
+++ main.js
@@ -0,0 +1,147 @@
+/**
+ * Cash Register Object
+ * @constructor
+ */
+var CashRegister  = function() {
+
+    /**
+     * The initial starting drawer
+     * @type {object}
+     */
+    var initialDrawer = [
+        {
+            'bill': '100',
+            'amount':   10
+        },
+        {
+            'bill': '50',
+            'amount':   10
+        },
+        {
+            'bill': '20',
+            'amount':   10
+        },
+        {
+            'bill': '10',
+            'amount':   10
+        },
+        {
+            'bill': '5',
+            'amount':   10
+        },
+        {
+            'bill': '1',
+            'amount':   10
+        }
+    ];
+
+    /**
+     * Populate drawer the first run
+     * @type {Object}
+     */
+    var currentBalance = initialDrawer;
+
+    /**
+     * Get current balance
+     * @returns {*}
+     */
+    this.getCurrentBalance = function() {
+        return currentBalance;
+    };
+
+    /**
+     * Get specific balances
+     *
+     * @param bills
+     * @returns {Array}
+     */
+    this.getSpecificBalances = function(bills) {
+        var balances = [];
+        for (var i = 0, count = bills.length; i < count; i++) {
+            var bill = bills[i].replace(/\D/g, '');
+            var potential = currentBalance.filter(function(billContainer) {
+                return billContainer.bill == bill;
+            });
+            if (!potential.length) throw Error('The bill ' + bill + ' was not found');
+            balances.push(potential.pop());
+        }
+
+        return balances;
+    };
+
+    /**
+     * Resets the drawer
+     */
+    this.reloadDrawer = function() {
+        currentBalance = initialDrawer;
+    };
+};
+
+
+/**
+ * Namespaced "container"
+ */
+var UserInterface = {
+    cashRegister: new CashRegister(),
+    form: document.querySelector('#handler'),
+    input: document.querySelector('#input'),
+    output: document.querySelector('#output'),
+
+    /**
+     * Launches this thing
+     */
+    init: function() {
+        var ui = this;
+        ui.form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            var value = this.input.value.toUpperCase();
+            ui.output.innerHTML = ui.handleCommand(value);
+            ui.input.value = '';
+        });
+    },
+
+    /**
+     * Handles the incoming command
+     * @param command
+     */
+    handleCommand: function(command) {
+        var output = '';
+        switch (command.charAt(0)) {
+            case 'R':
+                this.cashRegister.reloadDrawer();
+                output = this.formatBalance(this.cashRegister.getCurrentBalance(), ['Machine balance:']);
+                break;
+            case 'I':
+                try {
+                    output = this.formatBalance(this.cashRegister.getSpecificBalances(command.substr(2).split(' ')), []);
+                }
+                catch (e) {
+                    output = 'Failure: ' + e.message;
+                }
+                break;
+
+            default:
+                output = 'Failure: Invalid Command';
+
+        }
+
+        return output;
+
+    },
+
+    /**
+     * Formats the balance for output
+     * @param balance
+     * @param output
+     * @returns {string}
+     */
+    formatBalance: function(balance, output) {
+        for (var i = 0, count = balance.length; i < count; i++) {
+            output.push('$' + balance[i].bill + ' - ' + balance[i].amount);
+        }
+        return output.join('<br>');
+    }
+};
+
+
+UserInterface.init();
